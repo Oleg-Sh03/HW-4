42530d53d64aa73b2dfc578da574bc1b674fd197
Oleg Shnyrev <o-shnyrov@mail.ru>
2023-01-29 23:26:47 +0300

    Добавлены решения задач

---
+++ Task25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int Degree(int k, int g)
+{
+    int i = 1;
+    int num = 1;
+    while (i <= g)
+    {
+    num = num * k;
+    i++;        
+    }
+    return num;
+}
+int degree = Degree(a, b);
+Console.WriteLine(degree);
+
---
+++ Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int Sum(int num)
+{
+    int summa = 0;
+    while (num > 0)
+    {
+    int remains = num % 10;
+    num = (num - remains) / 10;
+    summa = summa + remains;
+    }
+    return summa;
+}
+int sum = Sum(number);
+Console.WriteLine(sum);
+
